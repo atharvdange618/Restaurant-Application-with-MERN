5c9796040fdea49be8a5fa24694ec514b1189c22
Atharv Dange <atharvdange618@gmail.com>
2024-05-20 18:05:55 +0530

    style: fixed tyops in dbConnection function

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -5,7 +5,7 @@ export const dbConnection = () => {
     mongoose.connect(process.env.MONGODB_URI, {
         dbName: "RESTAURANT_APPLICATION"
     }).then(() => {
-        console.log("Connected to the database daddy!")
+        console.log("Connected to the database")
     }).catch((err) => {
         console.error(err.message)
     });
